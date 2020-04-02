14fd11938ff74b94ce5a85c5dadffd21a5bca1d9
kondrashovsv <41994386+kondrashovsv@users.noreply.github.com>
2020-04-02 23:15:08 +0300

    Update gethwparam.sh

--- scripts/gethwparam.sh
+++ scripts/gethwparam.sh
@@ -3,10 +3,11 @@
 fnlog="log.txt"
 
 echo "Host name: " `hostname` > $fnlog
-echo "OS version: " `lsb_release -d | cut -d$'\t' -f2`
-echo "CPU cores:" `cat /proc/cpuinfo | grep processor | wc -l`
-echo "RAM: " `free -t -h | grep Mem |'awk '{print $2}'`
-echo "HDD size: " `df -h / | sed -n '1!p' | awk 'print $2}'` 
-echo "HDD disk usage: " `df -h / | sed -n '1!p' | awk 'print $2}'`
+echo "OS version: " `lsb_release -d | cut -f2` >> $fnlog
+echo "CPU cores:" `cat /proc/cpuinfo | grep processor | wc -l` >> $fnlog
+echo "RAM: " `free -t -h | grep Mem | awk '{print $2}'` >> $fnlog
+echo "HDD size: " `df -h / | sed -n '1!p' | awk '{print $2}'` >> $fnlog
+echo "HDD disk usage: " `df -h / | sed -n '1!p' | awk '{print $3}'` >> $fnlog
+
 
 echo "Hello from master branch"
