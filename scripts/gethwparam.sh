4ec20c26fd7e979a036bee104268d22e8814eea9
kondrashovsv <41994386+kondrashovsv@users.noreply.github.com>
2020-04-02 20:27:18 +0300

    added OS version

--- scripts/gethwparam.sh
+++ scripts/gethwparam.sh
@@ -2,6 +2,11 @@
 
 fnlog="log.txt"
 
-echo "HOSTNAME: " `hostname` > $fnlog
+echo "Host name: " `hostname` > $fnlog
+echo "OS version: " `lsb_release -d | cut -d$'\t' -f2`
+echo "CPU cores:"
+echo "RAM: "
+echo "HDD size: "
+echo "HDD disk usage: "
 
 echo "Hello from master branch"
